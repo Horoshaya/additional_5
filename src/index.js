26bb852e4c8891b3272a9b5abd568f1b666912e0
Дарья <daria@MBP-Dara.lan>
2018-09-18 19:07:51 +0300

    almost done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+			var count = 0;
+			str = str.split('');
+			pair1 = ['(', ')'];
+			pair2 = ['[', ']'];
+			pair3 = ['{', '}'];
+			pair4 = ['|', '|'];
+
+			var col = 0;
+
+
+			for (var i = 0; str.length-1 >= i; i++) {
+				var pair = [];
+
+				pair[0] = str[i];
+
+				for (var j = i+1; str.length-1 >= j; j++) {
+					pair[1] = str[j];
+
+					if (JSON.stringify(pair)==JSON.stringify(pair1) || JSON.stringify(pair)==JSON.stringify(pair2) || JSON.stringify(pair)==JSON.stringify(pair3) || JSON.stringify(pair)==JSON.stringify(pair4)) {
+
+						delete str[i];
+						delete str[j];
+
+						col = j - i - 1;
+						count++;
+
+						if (col % 2 != 0) {
+							return false;
+						} else break;
+					}
+				}	
+			}
+
+			if (str.length/count == 2 ) {
+				return true;
+			} else {
+				return false;
+			}
+}	
+
